7400f6f6a0e2538d25fff166c7337c873a94824a
typ99 <superarkada@gmail.com>
2023-02-17 04:37:17 +0300

    some fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
         alt='Troll face'
       />
       <h2 className='header--title'>Meme Generator</h2>
-      <h4 className='header--project'>React Course - Project 3</h4>
+      <h4 className='header--project'>typ99</h4>
     </header>
   );
 }
